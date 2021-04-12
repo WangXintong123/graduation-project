1ec90b0133f07325141f9f9e4efb815e6fe4efb3
wanglinawln <1416978302@qq.com>
2021-04-12 20:06:27 +0800

    authorize

--- project/miniprogram/pages/authorize/authorize.js
+++ project/miniprogram/pages/authorize/authorize.js
@@ -1,71 +1,76 @@
 // miniprogram/pages/authorize/authorize.js
+
 const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     isHide: false,
     userId: '',
+    isLogin:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-      // 查看是否授权
-      wx.getSetting({
-          success: function(res) {
-              if (res.authSetting['scope.userInfo']) {
-                  wx.getUserInfo({
-                      success: function(res) {
-                        wx.cloud.callFunction({
-                          name: 'login',
-                          data: {},
-                          success: res => {
-                            console.log('[云函数] [login] user openid: ', res.result.openid)
-                            console.log('调用login云函数返回的res',res)
-                            app.globalData.openid = res.result.openid;
-                            
-                          }
-                        })
-                      }
-                    });
+    //获取openid
+
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        console.log('调用login云函数返回的res',res)
+        app.globalData.openid = res.result.openid;
+        // 查看是否授权
+        const db = wx.cloud.database();
+          db.collection('users').get({
+            success: res => {
+              console.log(res.data);
+              console.log(app.globalData.openid)
+              for (var i = 0; i < res.data.length; i++) {
+                if (res.data[i]._openid == app.globalData.openid) {
+                  this.setData({
+                    isLogin:true,
+                    isHide:false
+                  },()=>{
                     wx.switchTab({
                       url: '../shouye/shouye',
                     })
-                    
-                } else {
-                    // 用户没有授权
-                    // 改变 isHide 的值，显示授权页面
-                    that.setData({
-                        isHide: true
-                    });
-                    
+                  })
                 }
+              }
+              if(!this.data.isLogin){
+                this.setData({
+                  isHide:true
+                })
+              }
             }
-        });
+          })
+      }
+    })
     },
-    bindGetUserInfo: function(e) {
-      if (e.detail.userInfo) {
+    getUserProfile: function(e) {
+      wx.getUserProfile({
+        desc: '用于完善用户资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
           //用户按了允许授权按钮
-          var that = this;
+          
           // 获取到用户的信息了，打印到控制台上看下
           console.log("用户的信息如下：");
-          console.log(e.detail.userInfo);
+          console.log(res.userInfo);
           //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
-          that.setData({
-              isHide: false
+          this.setData({
+              isHide: false,
           });
           const db = wx.cloud.database()
             db.collection('users').add({
             data: {
-                nickName:e.detail.userInfo.nickName,
-                avatarUrl:e.detail.userInfo.avatarUrl
+                nickName:res.userInfo.nickName,
+                avatarUrl:res.userInfo.avatarUrl
             },
             success: res => {
                 // 在返回结果中会包含新创建的记录的 _id
@@ -74,25 +79,27 @@ Page({
                 });
                 app.globalData.openid=res._openid
             }
-        });
-        wx.switchTab({
-          url: '../shouye/shouye',
-        })
-      } else {
+          });
+          wx.switchTab({
+            url: '../shouye/shouye',
+          })
+        },
+        fail:err=>{
           //用户按了拒绝按钮
           wx.showModal({
-              title: '警告',
-              content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-              showCancel: false,
-              confirmText: '返回授权',
-              success: function(res) {
-                  // 用户没有授权成功，不需要改变 isHide 的值
-                  if (res.confirm) {
-                      console.log('用户点击了“返回授权”');
-                  }
-              }
+            title: '警告',
+            content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+            showCancel: false,
+            confirmText: '返回授权',
+            success: function(res) {
+                // 用户没有授权成功，不需要改变 isHide 的值
+                if (res.confirm) {
+                    console.log('用户点击了“返回授权”');
+                }
+            }
           });
-      }
+        }
+      })
   },
 
   /**
